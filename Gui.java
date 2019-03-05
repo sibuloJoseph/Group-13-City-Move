29eb4ed65ed75c551f2e61c414cb2d2c3dffe704
KeatonBuddy <47367626+KeatonBuddy@users.noreply.github.com>
2019-03-05 16:29:02 -0700

    Update Gui.java

--- Gui.java
+++ Gui.java
@@ -3,30 +3,70 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.HBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.geometry.Pos;
+import javafx.scene.text.Font;
+import javafx.scene.control.PasswordField;
+import javafx.scene.paint.*;
+
 
 public class Gui extends Application {
+  private UserAccountList userAccountList;
   
   public void start(Stage primaryStage) throws Exception{
-      
-      	FlowPane flow = new FlowPane();
-      	flow.setAlignment(Pos.CENTER);
+     	VBox centerBox = new VBox();
+    	centerBox.setAlignment(Pos.CENTER);
+    
+        final ImageView selectedImage = new ImageView();   
+        Image image1 = new Image(Gui.class.getResourceAsStream("Logo.jpg"));
+		selectedImage.setImage(image1);
+		centerBox.getChildren().add(selectedImage);
+    
     	Label logInUsername = new Label ("Username:");
   		Label logInPassword = new Label ("Password:");
         TextField txtUsername = new TextField();
-        TextField txtPassword = new TextField();
-      	Button enterToAccount = new Button("Enter");
-          
-          
-          
-        Scene scene = new Scene(flow,400,200);
+        PasswordField txtPassword = new PasswordField();
+    	txtUsername.setMaxWidth(200);
+    	txtPassword.setMaxWidth(200);
+      	Button enterToAccount = new Button("Login");
+    
+    	centerBox.setSpacing(20);	
+      	logInUsername.setFont(Font.font("Verdana", 20));
+      	logInPassword.setFont(Font.font("Verdana", 20));
+    	logInUsername.setTextFill(Color.WHITE);
+    	logInPassword.setTextFill(Color.WHITE);
+    
+        centerBox.getChildren().addAll(logInUsername, txtUsername, logInPassword, txtPassword, enterToAccount);
+    	centerBox.setStyle("-fx-background-color: #980E0E;");
+    	
+    	enterToAccount.setOnAction(new EventHandler <ActionEvent>() {
+        	public void handle (ActionEvent event) {
+            	String username = txtUsername.getText();
+              	String password = txtPassword.getText();
+            	if(userAccountList.hasAccount(username)){
+                  if(userAccountList.credentialsValid(username, password)){
+                    primaryStage.setScene()
+                  }
+                }
+                else {
+                	userAccountList.addAccount(username, password);
+                }
+              	
+        });
+        
+                      
+    
+    
+        Scene sceneForLogin = new Scene(centerBox, 300,500);
         primaryStage.setTitle("Study Spots App");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(sceneForLogin);
         primaryStage.show();  
     }
+}
