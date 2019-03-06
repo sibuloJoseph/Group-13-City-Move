4f5a0be0799fdc269e1ad1b4d89af723e8e25cb4
sibuloJoseph <47368508+sibuloJoseph@users.noreply.github.com>
2019-03-05 22:27:00 -0700

    Updated 3/5 10:30pm
    Updated so you can sign out and get redirected back into the log in page.

--- Gui.java
+++ Gui.java
@@ -116,3 +116,130 @@ public class Gui extends Application {
         primaryStage.show();  
     }
 }
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.geometry.Pos;
+import javafx.scene.text.Font;
+import javafx.scene.control.PasswordField;
+import javafx.scene.paint.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+
+public class Gui extends Application {
+  private UserAccountList userAccountList = new UserAccountList();
+  
+  public void start(Stage primaryStage) throws Exception{
+        //User Log in interface.
+     	VBox centerBox = new VBox();
+    	centerBox.setAlignment(Pos.CENTER);
+    
+        final ImageView selectedImage = new ImageView();   
+        Image image1 = new Image(Gui.class.getResourceAsStream("Logo.jpg"));
+		selectedImage.setImage(image1);
+		centerBox.getChildren().add(selectedImage);
+    
+        Label output = new Label ("");
+    	Label logInUsername = new Label ("Username:");
+  		Label logInPassword = new Label ("Password:");
+        TextField txtUsername = new TextField();
+        PasswordField txtPassword = new PasswordField();
+    	txtUsername.setMaxWidth(200);
+        txtPassword.setMaxWidth(200);
+        
+        HBox loginSignup = new HBox();
+        loginSignup.setAlignment(Pos.CENTER);
+        Button enterToAccount = new Button("Login");
+        Button signupToAccount = new Button("Signup");
+        loginSignup.getChildren().addAll(enterToAccount, signupToAccount);
+
+
+    	centerBox.setSpacing(20);	
+      	logInUsername.setFont(Font.font("Verdana", 20));
+      	logInPassword.setFont(Font.font("Verdana", 20));
+    	logInUsername.setTextFill(Color.WHITE);
+        logInPassword.setTextFill(Color.WHITE);
+        output.setTextFill(Color.WHITE);
+    
+        centerBox.getChildren().addAll(logInUsername, txtUsername, logInPassword, txtPassword, loginSignup, output);
+        centerBox.setStyle("-fx-background-color: #980E0E;");
+        
+        //Main Menu Interface
+        final ImageView mapImage = new ImageView();   
+        Image image2 = new Image(Gui.class.getResourceAsStream("UofCMap.png"));
+        mapImage.setImage(image2);
+        VBox mainMenuGUI = new VBox();
+        mainMenuGUI.setAlignment(Pos.CENTER);
+        mainMenuGUI.setSpacing(15);
+        Button surveyButton = new Button("Do Survey");
+        Button pastButton = new Button("See Previuos Study Spots");
+        Button signout = new Button("Sign Out");
+        mainMenuGUI.getChildren().addAll(mapImage, surveyButton, pastButton, signout);
+        mainMenuGUI.setStyle("-fx-background-color: #980E0E;");
+
+        Scene sceneForMainMenu = new Scene(mainMenuGUI);
+        Scene sceneForLogin = new Scene(centerBox, 300,500);
+
+
+    	
+    	enterToAccount.setOnAction(new EventHandler<ActionEvent>() {
+        	public void handle (ActionEvent event) {
+            	String username = txtUsername.getText();
+                String password = txtPassword.getText();
+                  
+                if(userAccountList.credentialsValid(username, password)){
+                    primaryStage.setScene(sceneForMainMenu);
+                    primaryStage.setTitle("Main Menu");
+                }
+                else {
+                    output.setText("Invalid Username or Password.");
+                }
+            }
+        });
+
+        signupToAccount.setOnAction(new EventHandler<ActionEvent>(){
+            public void handle (ActionEvent event) {
+            	String username = txtUsername.getText();
+                String password = txtPassword.getText();
+                  
+            	if(userAccountList.hasAccount(username)){
+                    output.setText("Account Already Exist!");
+                }
+                else {
+                    if(username.isEmpty() || password.isEmpty()){
+                        output.setText("Please Enter Username and/or Password.");
+                    }
+                    else{
+                        userAccountList.addAccount(username, password);
+                        output.setText("Account Created \n Please Log In.");
+                    }
+                }
+            }
+        });
+
+        signout.setOnAction(new EventHandler<ActionEvent>(){
+            public void handle (ActionEvent event){
+                primaryStage.setScene(sceneForLogin); 
+                txtPassword.setText("");
+            }
+        });
+
+        
+        primaryStage.setTitle("Study Spots App");
+        primaryStage.setScene(sceneForLogin);
+        primaryStage.show();  
+    }
+}
