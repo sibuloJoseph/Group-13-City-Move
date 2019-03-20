4617c9ec64d8bbb22e8aa7e3b9196cc8dec0de9b
Josh Diwa <47367644+joshdiwa@users.noreply.github.com>
2019-03-19 19:55:24 -0600

    Fixed Grammar Error in "Account Already Exist!"

--- Gui.java
+++ Gui.java
@@ -142,7 +142,7 @@ public class Gui extends Application {
         password = txtPassword.getText();
             
         if (userAccountList.hasAccount(username)) {
-            output.setText("Account Already Exist!");
+            output.setText("Account Already Exists!");
         }
         else {
             if (username.isEmpty() || password.isEmpty()) {
