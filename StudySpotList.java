9d998128a8aa00bea8e231f99cd130d547160947
sibuloJoseph <47368508+sibuloJoseph@users.noreply.github.com>
2019-03-24 23:09:57 -0600

    All Tests Passed

--- StudySpotList.java
+++ StudySpotList.java
@@ -13,7 +13,7 @@ public class StudySpotList{
     private IdealStudySpot userIdeal = new IdealStudySpot();
     private ArrayList<StudySpot> studySpotList = new ArrayList<StudySpot>();
     private Scanner studySpotsFromTxt;
-    private StudySpot studySpot =new StudySpot();
+    private StudySpot studySpot = new StudySpot();
 
     /**
      * Default constructor for the StudySpotList object
@@ -22,7 +22,6 @@ public class StudySpotList{
         try {
             studySpotsFromTxt = new Scanner(new File("StudySpotsListV1.0.0.txt"));
             while (studySpotsFromTxt.hasNext()){
-
                 studySpot = new StudySpot();
                 studySpot.setName(studySpotsFromTxt.nextLine());
                 studySpot.setNoiseLevel(Double.parseDouble(studySpotsFromTxt.nextLine()));
@@ -34,11 +33,10 @@ public class StudySpotList{
                 studySpot.setX2(Double.parseDouble(studySpotsFromTxt.nextLine()));
                 studySpot.setY1(Double.parseDouble(studySpotsFromTxt.nextLine()));
                 studySpot.setY2(Double.parseDouble(studySpotsFromTxt.nextLine()));
-                this.getStudySpotList().add(studySpot);
+                studySpotList.add(studySpot);
             }
         }
         catch (Exception e) {
-            //TODO: handle exception
             System.out.println("No File"); 
             System.exit(0);
         }
@@ -58,52 +56,59 @@ public class StudySpotList{
      * Sets the userIdeal object to the given IdealStudySpot parameter.   
      * @param userIdeal variable from the IdealStudySpot class.
      */
-    public void setUserIdeal(IdealStudySpot userIdeal){
-        this.userIdeal = userIdeal;
+    public void setUserIdeal(IdealStudySpot sUserIdeal){
+        if(sUserIdeal == null){
+            userIdeal = userIdeal;
+        }
+        else{
+            userIdeal = new IdealStudySpot(sUserIdeal);
+        }
     }
 
     /**
      * Returns the ideal StudySpot based on the user's input.
      */
     public IdealStudySpot getUserIdeal(){
-        return this.userIdeal;
+        return new IdealStudySpot(userIdeal);
     }
 
     /**
      * Returns the list of StudySpots after getting the study spots from the StudySpotsList text file.
      */
     public ArrayList<StudySpot> getStudySpotList(){
-        return this.studySpotList;
-
+        StudySpotList s = new StudySpotList();
+        return new ArrayList<StudySpot>(s.studySpotList);
     }
 
     /**
      * Returns the best and  most ideal study spots based on the user's input.
      */
     public ArrayList<StudySpot> getBestStudySpots(){
+        double temp;
+        StudySpot tempss;
+        StudySpotList s = new StudySpotList();
+        
         //Create an ArrayList for the comparison values from the compareTo() method in the IdealStudySpot Class
         ArrayList<Double> comparisonValues = new ArrayList<Double>();
-        for(int x=0; x<this.getStudySpotList().size(); x++){
-            comparisonValues.add(this.getUserIdeal().compareTo(new StudySpot(this.getStudySpotList().get(x))));
+        for(int x=0; x<s.studySpotList.size(); x++){
+            comparisonValues.add(this.getUserIdeal().compareTo(new StudySpot(s.studySpotList.get(x))));
         }
         
         //Sort the comparisonValues array in ascending order and match it with the studySpotList array so that 
         //the studySpotList array will have the best study spot in the first index. 
-        double temp;
-        StudySpot tempss;
-        for(int i = 1; i < this.getStudySpotList().size(); i++) {
+        for(int i = 1; i < s.studySpotList.size(); i++) {
             for(int j = i - 1; j >= 0; j--) {
                 if(comparisonValues.get(j) > comparisonValues.get(j+1)) {
                     temp = comparisonValues.get(j);
                     comparisonValues.set(j, comparisonValues.get(j+1));
                     comparisonValues.set(j+1, temp);
-                    tempss = this.getStudySpotList().get(j);
-                    this.getStudySpotList().set(j, this.getStudySpotList().get(j+1));
-                    this.getStudySpotList().set(j+1, tempss);
+                    tempss = s.studySpotList.get(j);
+                    s.studySpotList.set(j, s.studySpotList.get(j+1));
+                    s.studySpotList.set(j+1, tempss);
                 }
             }
         }
-        return this.getStudySpotList();
+        return new ArrayList<StudySpot>(s.studySpotList);
     }
 
     public StudySpot getLocation(double x, double y){
@@ -112,11 +117,9 @@ public class StudySpotList{
             if(studySpotList.get(i).getX1()<x && studySpotList.get(i).getX2()>x){
                 if(studySpotList.get(i).getY1()<y && studySpotList.get(i).getY2()>y){
                     s = new StudySpot(studySpotList.get(i));
-                    break;
                 }
             }
         }
-        
         return s;
     }
 
