78876392d2cd67e74d89a31e02a88d4c7ec5481f
Braydon Willms <47362707+braydonwillms@users.noreply.github.com>
2019-04-09 12:42:43 -0600

    Braydon: Updated Schedule to work with five days

--- logic/Schedule.java
+++ logic/Schedule.java
@@ -2,7 +2,7 @@
  * This defines the Schedule class which is meant to describe a user's class shedule and
  * recommend spots based on it
  *
- * Last Modified: April 6, 2019
+ * Last Modified: April 8, 2019
  *
  */
 
@@ -14,15 +14,17 @@ import java.lang.Math;
 import java.util.ArrayList;
 
 public class Schedule implements Serializable{
-    private StudySpot[] day1Schedule; // each element represents an hour of the day
+    private StudySpot[][] weekSchedule; // each row represents a day and each element represents an hour
 
     /**
      * Default constructor for Schedule object which creates an empty Schedule
      */
     public Schedule() {
-        day1Schedule = new StudySpot[15];
-        for(int i = 0;i < 15;i++) {
-            day1Schedule[i] = null;
+        weekSchedule = new StudySpot[5][15];
+        for(int i = 0;i < 5;i++) {
+            for(int j = 0;j < 15;j++) {
+                weekSchedule[i][j] = null;
+            }
         }
     }
 
@@ -30,19 +32,22 @@ public class Schedule implements Serializable{
      * Copy constructor for Schedule object
      */
     public Schedule(Schedule scheduleToCopy) {
-        this.day1Schedule = new StudySpot[15];
-        for(int i = 8;i < 21;i++) {
-            this.setClass(i, scheduleToCopy.getClass(i));
+        this.weekSchedule = new StudySpot[5][15];
+        for(int i = 1;i < 6;i++) {
+            for(int j = 8;j < 21;j++) {
+                this.setClass(i, j, scheduleToCopy.getClass(i, j));
+            }
         }
     }
 
     /**
      * Returns the StudySpot object representing the class location at the given time or null if there isn't one
+     * @param day: int representing the day, from 1 for Monday to 5 for Friday
      * @param timeIn24Hour: int representing the hour of the day, between 8:00 and 20:00 inclusive, to get the class at
      */
-    public StudySpot getClass(int timeIn24Hour){
-        if((timeIn24Hour >= 8 && timeIn24Hour <= 20) && day1Schedule[timeIn24Hour - 7] != null) {
-            return new StudySpot(day1Schedule[timeIn24Hour - 7]);
+    public StudySpot getClass(int day, int timeIn24Hour){
+        if((day >= 1 && day <= 5) && (timeIn24Hour >= 8 && timeIn24Hour <= 20) && weekSchedule[day-1][timeIn24Hour - 7] != null) {
+            return new StudySpot(weekSchedule[day-1][timeIn24Hour - 7]);
         }
         else {
             return null;
@@ -51,17 +56,18 @@ public class Schedule implements Serializable{
 
     /**
      * Sets the class location at the given time to the given StudySpot object
+     * @param day: int representing the day, from 1 for Monday to 5 for Friday
      * @param timeIn24Hour: int representing the hour of the day, between 8:00 and 20:00 inclusive, to add the class at
      * @param studySpotToAdd: StudySpot object representing the location of the class at the given time
      */
-    public void setClass(int timeIn24Hour, StudySpot studySpotToAdd){
+    public void setClass(int day, int timeIn24Hour, StudySpot studySpotToAdd){
 
-        if(timeIn24Hour >= 8 && timeIn24Hour <= 20) {
+        if(day >= 1 && day <= 5 && timeIn24Hour >= 8 && timeIn24Hour <= 20) {
             if(studySpotToAdd != null) {
-                day1Schedule[timeIn24Hour - 7] = new StudySpot(studySpotToAdd);
+                weekSchedule[day-1][timeIn24Hour - 7] = new StudySpot(studySpotToAdd);
             }
             else {
-                day1Schedule[timeIn24Hour - 7] = null;
+                weekSchedule[day-1][timeIn24Hour - 7] = null;
             }
         }
     }
@@ -70,21 +76,22 @@ public class Schedule implements Serializable{
      * Returns a double value describing the average distance between the given StudySpot and the classes surrounding
      * the given time
      * @param studySpotToCompare: StudySpot object to check for distance to classes
+     * @param day: int representing the day to check around, from 1 for Monday to 5 for Friday
      * @param hour: int representing the the time in 24 hours to check around
      * @param numOfSurroundingSpots: number of classes immediately on either side of the given time
      */
-    private double compareDistance(StudySpot studySpotToCompare, int hour, int numOfSurroundingSpots){
+    private double compareDistance(StudySpot studySpotToCompare, int day, int hour, int numOfSurroundingSpots){
 
         double averageDistance = 0.0;
 
         // equations are simply pythagorean theorem
-        if(day1Schedule[hour - 8] != null) {
-            averageDistance += Math.sqrt((day1Schedule[hour - 8].averageX() - studySpotToCompare.averageX()) * (day1Schedule[hour - 8].averageX() - studySpotToCompare.averageX()) +
-                    (day1Schedule[hour - 8].averageY() - studySpotToCompare.averageY()) * (day1Schedule[hour - 8].averageY() - studySpotToCompare.averageY()));
+        if(weekSchedule[day-1][hour - 8] != null) {
+            averageDistance += Math.sqrt((weekSchedule[day-1][hour - 8].averageX() - studySpotToCompare.averageX()) * (weekSchedule[day-1][hour - 8].averageX() - studySpotToCompare.averageX()) +
+                    (weekSchedule[day-1][hour - 8].averageY() - studySpotToCompare.averageY()) * (weekSchedule[day-1][hour - 8].averageY() - studySpotToCompare.averageY()));
         }
-        if(day1Schedule[hour - 6] != null) {
-            averageDistance += Math.sqrt((day1Schedule[hour - 6].averageX() - studySpotToCompare.averageX()) * (day1Schedule[hour - 6].averageX() - studySpotToCompare.averageX()) +
-                    (day1Schedule[hour - 6].averageY() - studySpotToCompare.averageY()) * (day1Schedule[hour - 6].averageY() - studySpotToCompare.averageY()));
+        if(weekSchedule[day-1][hour - 6] != null) {
+            averageDistance += Math.sqrt((weekSchedule[day-1][hour - 6].averageX() - studySpotToCompare.averageX()) * (weekSchedule[day-1][hour - 6].averageX() - studySpotToCompare.averageX()) +
+                    (weekSchedule[day-1][hour - 6].averageY() - studySpotToCompare.averageY()) * (weekSchedule[day-1][hour - 6].averageY() - studySpotToCompare.averageY()));
         }
         averageDistance = averageDistance / numOfSurroundingSpots;
 
@@ -102,29 +109,30 @@ public class Schedule implements Serializable{
         int numOfSurroundingSpots = 0;
 
         int hour = LocalDateTime.now().getHour();
-        if(hour < 8 || hour > 20) {
+        if(hour < 8) {
             hour = 8;
         }
 
-        // finds the next time the user is free to study, looping to the next morning if
-        // there are no times remaining in the current day
-        for(int i = 1;i < 14;i++) {
-            if(day1Schedule[hour - 7] == null) {
-                if(day1Schedule[hour - 8] != null) {
+        int day = LocalDateTime.now().getDayOfWeek().getValue();
+        if(day > 5) {
+            day = 1;
+            hour = 8;
+        }
+
+        // finds the next time the user is free to study in the current day if there is one
+        for( ;hour < 21;hour++) {
+            if(weekSchedule[day-1][hour - 7] == null) {
+                if(weekSchedule[day-1][hour - 8] != null) {
                     numOfSurroundingSpots++;
                 }
-                if(day1Schedule[hour - 6] != null) {
+                if(weekSchedule[day-1][hour - 6] != null) {
                     numOfSurroundingSpots++;
                 }
             }
-            if(numOfSurroundingSpots > 0 || day1Schedule[hour - 7] == null) {
+            if(numOfSurroundingSpots > 0 || weekSchedule[day-1][hour - 7] == null) {
                 break;
             }
 
-            hour++;
-            if(hour > 20) {
-                hour = 8;
-            }
         }
 
         ArrayList<StudySpot> bestSpotList = spotList.getBestStudySpots();
@@ -133,7 +141,7 @@ public class Schedule implements Serializable{
         // list is not rearranged if the user has no classes or no availability
         if(numOfSurroundingSpots > 0) {
             for (int i = 0; i < bestSpotList.size(); i++) {
-                distances.add(this.compareDistance(bestSpotList.get(i), hour, numOfSurroundingSpots));
+                distances.add(this.compareDistance(bestSpotList.get(i), day, hour, numOfSurroundingSpots));
             }
 
             Double temp;
